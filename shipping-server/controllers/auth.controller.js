268b7ad058325d6715eeeef2e0d42dc9fe9c6869
kemi Chris <ekeminichristopher123@gmail.com>
2026-09-02 20:05:10 +0100

    feat: add login functionality and user role to user model

--- shipping-server/controllers/auth.controller.js
+++ shipping-server/controllers/auth.controller.js
@@ -12,3 +12,17 @@ export const register = async (req, res, next) => {
         next(error);
     }
 };
+
+export const login = async (req, res, next) => {
+    try {
+        const loggedIn = await authServices.login(req.body);
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successful.",
+            data: loggedIn,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- shipping-server/routes/auth.routes.js
+++ shipping-server/routes/auth.routes.js
@@ -3,5 +3,6 @@ import * as authController from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register', authController.register)
+router.post('/login', authController.login)
 
 export default router
